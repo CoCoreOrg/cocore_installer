b94b6aeeb089155bb0d4852dc20ad678d64cb63e
Devin Gaffney <itsme@devingaffney.com>
2024-08-02 16:41:06 -0700

    updates

--- setup.py
+++ setup.py
@@ -1,20 +1,23 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cocore_installer",
-    version="0.1",
+    name='cocore_installer',
+    version='0.1',
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+        'cocore_installer': ['firecracker_config.json'],
+    },
     install_requires=[
-        "requests",
-        "cryptography",
-        "websockets"
+        'requests',
+        'cryptography',
+        'websockets',
     ],
     entry_points={
         'console_scripts': [
             'cocore-install=cocore_installer.install:main',
-            'cocore-setup-firecracker=cocore_installer.setup_firecracker:main',
             'cocore-store-auth-key=cocore_installer.store_auth_key:main',
+            'cocore-setup-firecracker=cocore_installer.setup_firecracker:main',
         ],
     },
 )

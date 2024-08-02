020099e13731ed914c048a62fdb4e503a1408227
Devin Gaffney <itsme@devingaffney.com>
2024-08-02 16:04:41 -0700

    updates

--- cocore_installer/install.sh
+++ cocore_installer/install.sh
@@ -11,34 +11,32 @@ TASK_WORKER_SCRIPT="cocore_installer/task_worker.py"  # Correct path to task_wor
 
 # Install Firecracker and Jailer
 install_dir=/firecracker/releases
-bin_dir=/usr/bin
+bin_dir=/usr/local/bin
 release_url="https://github.com/firecracker-microvm/firecracker/releases/download/v${FIRECRACKER_VERSION}"
 arch=$(uname -m)
 
 if [ -d "${install_dir}/release-v${FIRECRACKER_VERSION}" ]; then
     echo "Firecracker ${FIRECRACKER_VERSION} already installed"
 else
-    mkdir -p "${install_dir}"
+    mkdir -p "${install_dir}/release-v${FIRECRACKER_VERSION}"
     download_url="${release_url}/firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz"
     echo "Attempting to download Firecracker from URL: ${download_url}"
     wget -O "${install_dir}/firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz" "${download_url}"
-    pushd "${install_dir}"
+    pushd "${install_dir}/release-v${FIRECRACKER_VERSION}"
 
-    echo "Decompressing firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz in ${install_dir}"
-    tar -xzf "firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz"
-    rm "firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz"
+    echo "Decompressing firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz in ${install_dir}/release-v${FIRECRACKER_VERSION}"
+    tar -xzf "${install_dir}/firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz"
+    rm "${install_dir}/firecracker-v${FIRECRACKER_VERSION}-${arch}.tgz"
 
-    echo "Linking firecracker ${FIRECRACKER_VERSION}-${arch}"
-    sudo ln -sfn "${install_dir}/firecracker-v${FIRECRACKER_VERSION}-${arch}" "${bin_dir}/firecracker-${FIRECRACKER_VERSION}-${arch}"
-    sudo ln -sfn "${install_dir}/jailer-v${FIRECRACKER_VERSION}-${arch}" "${bin_dir}/jailer-${FIRECRACKER_VERSION}-${arch}"
-    sudo ln -sfn "${bin_dir}/firecracker-${FIRECRACKER_VERSION}-${arch}" "${bin_dir}/firecracker"
-    sudo ln -sfn "${bin_dir}/jailer-${FIRECRACKER_VERSION}-${arch}" "${bin_dir}/jailer"
+    echo "Linking firecracker and jailer"
+    sudo ln -sfn "${install_dir}/release-v${FIRECRACKER_VERSION}/firecracker-v${FIRECRACKER_VERSION}-${arch}" "${bin_dir}/firecracker"
+    sudo ln -sfn "${install_dir}/release-v${FIRECRACKER_VERSION}/jailer-v${FIRECRACKER_VERSION}-${arch}" "${bin_dir}/jailer"
 
-    echo "firecracker ${FIRECRACKER_VERSION}-${arch}: ready"
-    ls -l "${bin_dir}/firecracker-${FIRECRACKER_VERSION}-${arch}"
-    file "${bin_dir}/firecracker-${FIRECRACKER_VERSION}-${arch}"
+    echo "firecracker and jailer ${FIRECRACKER_VERSION}-${arch}: ready"
+    ls -l "${bin_dir}/firecracker"
     file "${bin_dir}/firecracker"
-    firecracker --version | head -n1
+    file "${bin_dir}/jailer"
+    "${bin_dir}/firecracker" --version | head -n1
     popd
 fi
 

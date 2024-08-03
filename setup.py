e09025b244f40b67c75797c9edf25adc38649d1c
Devin Gaffney <itsme@devingaffney.com>
2024-08-02 17:01:30 -0700

    updates

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     },
     install_requires=[
         'requests == 2.24.0',
-        'urllib3==2.2.2',
+        'urllib3<1.26',
         'cryptography',
         'websockets',
         'requests-unixsocket == 0.2.0',

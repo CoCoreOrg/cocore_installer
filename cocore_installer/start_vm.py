e8652bf5af1acc1b28fe7b6b38d613ae69f72b31
Devin Gaffney <itsme@devingaffney.com>
2024-08-12 10:50:38 -0700

    hacking

--- cocore_installer/start_vm.py
+++ cocore_installer/start_vm.py
@@ -5,7 +5,7 @@ def main():
     vm_number = sys.argv[1]
     cpus = sys.argv[3]
     memory = sys.argv[5]
-
+    import code;code.interact(local=dict(globals(), **locals())) 
     subprocess.run(['bash', '-c', f'cocore_installer/start_vm.sh {vm_number} {cpus} {memory}'], check=True)
 
 if __name__ == "__main__":

70d72cf7c8ddcc79e6ff504dcad71ecc702fc1bd
Devin Gaffney <itsme@devingaffney.com>
2024-08-12 10:54:32 -0700

    hacking

--- cocore_installer/start_vm.py
+++ cocore_installer/start_vm.py
@@ -5,8 +5,8 @@ def main():
     vm_number = sys.argv[1]
     cpus = sys.argv[3]
     memory = sys.argv[5]
-    # Use bash -c correctly, with _ as $0 and the script arguments
-    subprocess.run(['bash', '-c', f'./cocore_installer/start_vm.sh "$0" "$1" "$2"', '_', vm_number, cpus, memory], check=True)
+    # Directly pass the script and its arguments without using bash -c
+    subprocess.run([f'./cocore_installer/start_vm.sh', vm_number, cpus, memory], check=True)
 
 if __name__ == "__main__":
     main()

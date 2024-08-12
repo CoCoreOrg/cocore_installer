f3dac1c171b98c687334719cd58bfc8c7b6e57fc
Devin Gaffney <itsme@devingaffney.com>
2024-08-12 11:06:39 -0700

    hacking

--- cocore_installer/start_vm.py
+++ cocore_installer/start_vm.py
@@ -1,9 +1,4 @@
 import subprocess
-import sys
 
 def main():
-    vm_number = sys.argv[1] if len(sys.argv) > 1 else ""
-    subprocess.run([f'./cocore_installer/start_vm.sh', vm_number], check=True)
-
-if __name__ == "__main__":
-    main()
+    subprocess.run(['bash', '-c', 'cocore_installer/start_vm.sh 1'], check=True)

dc68a2f622721f22e26660ea972ddbfd63739cdb
Devin Gaffney <itsme@devingaffney.com>
2024-08-12 09:26:40 -0700

    logging

--- cocore_installer/init.sh
+++ cocore_installer/init.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
+
+echo "Starting swap setup..." | tee /dev/kmsg
 swapon /root/swapfile
+echo "Swap setup complete." | tee /dev/kmsg
 
 set -e
 
-echo COCORE
+echo "Starting CoCore initialization..." | tee /dev/kmsg
 
+echo "Touching dpkg status file..." | tee /dev/kmsg
 touch /var/lib/dpkg/status
 
-# The task worker runs in the VM; the package manager will always be APT/Ubuntu
+echo "Updating apt-get package list..." | tee /dev/kmsg
 apt-get update -y
+
+echo "Installing python3-venv..." | tee /dev/kmsg
 apt-get install -y python3-venv
 
+echo "Setting up Python virtual environment..." | tee /dev/kmsg
 python3 -m venv /root/venv
 
+echo "Activating virtual environment..." | tee /dev/kmsg
 source /root/venv/bin/activate
 
+echo "Installing Python dependencies..." | tee /dev/kmsg
 pip install requests websockets cryptography psutil boto3 urllib3 setuptools requests botocore certifi charset-normalizer idna typing-extensions packaging python-dateutil cryptography pyyaml aiobotocore six s3transfer numpy grpcio-status fsspec scikit-learn
 
+echo "Running task_worker.py..." | tee /dev/kmsg
 python3 /root/task_worker.py
+
+echo "CoCore initialization complete." | tee /dev/kmsg

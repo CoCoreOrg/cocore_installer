e958dac438add15a36634cb7a9c48ab7808b48ed
Devin Gaffney <itsme@devingaffney.com>
2024-09-07 04:35:24 -0700

    Updates

--- cocore_installer/init.sh
+++ cocore_installer/init.sh
@@ -29,7 +29,9 @@ run_and_log "curl_install" "apt-get install -y curl"
 
 # Start swap setup
 log_status "swap_setup_started" "true" "Starting swap setup..."
-run_and_log "ls_start" "ls /root"
+run_and_log "ls_env" "env"
+run_and_log "ls_root" "ls /"
+run_and_log "ls_root_dir" "ls /root"
 run_and_log "swapon" "swapon /root/swapfile"
 
 # Start CoCore initialization
@@ -42,7 +44,7 @@ run_and_log "touch_dpkg" "touch /var/lib/dpkg/status"
 run_and_log "apt_get_update_2" "apt-get update -y"
 
 # Install system dependencies
-run_and_log "install_dependencies" "apt-get install -y tree python3-venv ffmpeg build-essential libjpeg-dev zlib1g-dev libpng-dev libpq-dev git curl wget maven openjdk-17-jdk ruby-full"
+run_and_log "install_dependencies" "apt-get install -y tree python3 python3-venv ffmpeg build-essential libjpeg-dev zlib1g-dev libpng-dev libpq-dev git curl wget maven openjdk-17-jdk ruby-full"
 
 # Clean up apt cache
 run_and_log "clean_apt_cache" "apt-get clean"
